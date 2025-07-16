38af6595d0dd377d5e746012e9b2464f7b6acc8b
Matvii Ulytskyi <matveyulitskiy@gmail.com>
2025-07-16 20:57:51 +0200

    feat(hw-13): finished subtask 3

--- lib/features/homeworks/lesson_13/homework_13_screen.dart
+++ lib/features/homeworks/lesson_13/homework_13_screen.dart
@@ -7,33 +7,95 @@ class HomeworkThirteenScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          width: 300,
-          height: 150,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.blue,
-          ),
-          alignment: Alignment.bottomRight,
-          child: const Padding(
-            padding: EdgeInsets.all(10),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              spacing: 10,
-              children: [
-                Icon(Icons.star, color: Colors.white),
-                Text(
-                  'Привіт, Flutter!',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
+        child: Column(
+          spacing: 8,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: 300,
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.blue,
+              ),
+              alignment: Alignment.bottomRight,
+              child: const Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  spacing: 10,
+                  children: [
+                    Icon(Icons.star, color: Colors.white),
+                    Text(
+                      'Привіт, Flutter!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Icon(Icons.star, color: Colors.white),
+                  ],
                 ),
-                Icon(Icons.star, color: Colors.white),
-              ],
+              ),
             ),
-          ),
+            Container(
+              width: 300,
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.green,
+              ),
+              alignment: Alignment.bottomRight,
+              child: const Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  spacing: 10,
+                  children: [
+                    Icon(Icons.star, color: Colors.white),
+                    Text(
+                      'Привіт, Flutter!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Icon(Icons.star, color: Colors.white),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.red,
+              ),
+              alignment: Alignment.bottomRight,
+              child: const Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  spacing: 10,
+                  children: [
+                    Icon(Icons.star, color: Colors.white),
+                    Text(
+                      'Привіт, Flutter!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Icon(Icons.star, color: Colors.white),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
