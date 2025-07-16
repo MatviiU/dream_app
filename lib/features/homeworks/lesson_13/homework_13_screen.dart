56b3f5065393a54fa5caa097b65163276c35d562
Matvii Ulytskyi <matveyulitskiy@gmail.com>
2025-07-16 20:38:09 +0200

    feat(hw-13): finished subtask 1

---
+++ lib/features/homeworks/lesson_13/homework_13_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeworkThirteenScreen extends StatelessWidget {
+  const HomeworkThirteenScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 150,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    );
+  }
+}
