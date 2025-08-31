1cdcb5841eb8394b354914b34963a00a889b7f45
Matvii Ulytskyi <matveyulitskiy@gmail.com>
2025-08-31 10:33:16 +0200

    refactor(hw-19): analysis warning fixed

--- lib/features/homeworks/lesson_19/homework_bloc/bloc/lesson_19_event.dart
+++ lib/features/homeworks/lesson_19/homework_bloc/bloc/lesson_19_event.dart
@@ -1,9 +1,9 @@
 part of 'lesson_19_bloc.dart';
 
-abstract class Lesson19Event{
+abstract class Lesson19Event {
   const Lesson19Event();
 }
 
-class Lesson19BlocIncrementEvent extends Lesson19Event{}
+class Lesson19BlocIncrementEvent extends Lesson19Event {}
 
-class Lesson19BlocDecrementEvent extends Lesson19Event{}
+class Lesson19BlocDecrementEvent extends Lesson19Event {}
